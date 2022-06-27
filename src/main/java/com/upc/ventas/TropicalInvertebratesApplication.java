18c1390e7ff90c5fb49b3768793793b23b2a16db
Jacob.cabrera <jacob.cabrera@outlook.com>
2022-06-27 00:47:37 -0500

    intento1

---
+++ src/main/java/com/upc/ventas/TropicalInvertebratesApplication.java
@@ -0,0 +1,13 @@
+package com.upc.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TropicalInvertebratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TropicalInvertebratesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/ventas/TropicalInvertebratesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TropicalInvertebratesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
